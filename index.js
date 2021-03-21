582ea5cdb9a0974fba0848983eaa19897650f70b
animacpac <56101332+animacpac@users.noreply.github.com>
2021-03-20 21:36:23 -0600

    test 2

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const { response } = require("express");
 var express = require("express");
 var app = express();
 
+app.get('', (req, res) =>{
+  res.send("This is weather app")
 
-app.set("port", (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.get('/', function(request, response) {
-  response.render('pages/index');
 });
 
+app.get('/weather', (req, res) =>{
+  res.send("This is weather end point")
+
+});
+
+app.get("*", (req, res) =>{
+  res.send("Page not found")
+
+});
 app.listen(app.get("port"), function(){
     console.log("listening for connection on port: ", app.get("port"));
 
