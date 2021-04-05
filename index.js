701913bd1eac8a11ad65c256f7ef7b5100b66fb5
animacpac <56101332+animacpac@users.noreply.github.com>
2021-04-04 22:02:04 -0600

    test

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const hbs = require("hbs");
 const path = require("path");
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config');
+
+
+initializePassport(
+ passport,
+ email => user.find(user => user.email === email),
+ id => user.find(user => user.id === id)
+ )
+const users = []
 
 const port = process.env.PORT || 3000
 
@@ -18,18 +37,32 @@ const viewsPath = path.join(__dirname, './views/pages');
 
 const partialsPath = path.join(__dirname, './views/partials');
 
+
+
 app.set('view engine', 'hbs', 'html');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
+app.use(express.urlencoded({ extended: false}))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.get('', (req, res) => {
-  res.render('index', {
+  res.render('index', { 
+    //name: req.user.name,
     title: 'Weather App'
   })
 })
 
 app.get('/weather', (req, res) => {
+
   const address = req.query.address
   if (!address) {
 
@@ -54,6 +87,39 @@ app.get('/weather', (req, res) => {
 
 app.get('/About', (req, res) => {
   res.render('about')
+});
+
+app.get('/Login', (req, res) => {
+  res.render('login')
+});
+
+app.get('/Register', (req, res) => {
+  res.render('register')
+});
+app.post('/Register',  async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+     name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/Login')
+  }
+  catch{
+    res.redirect('/Register')
+  }
+})
+app.post('/Login', passport.authenticate('local', {
+  successRedirect: '/', 
+  failureRedirect: '/Login',
+  failureFlash: true
+}) )
+
+app.delete('/Logout', (req, res) => {
+  req.logOut()
+  req.redirect('/Login')
 });
 
 app.get('/Goals', (req, res) => {
@@ -81,6 +147,20 @@ app.listen(port, () => {
   console.log("Server is working on port:", port);
 })
 
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/Login')
+}
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+
+  
+}
 
 
 
